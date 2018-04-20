733f34b413200f9ff7a8cc92887df5ac2b604301
jpakala <jpakala@andrew.cmu.edu>
2018-04-19 21:38:46 -0400

    run_simulation(n_trials, n, p, cutoff) which uses the previous two functions to run n_trials   simulations which uses data from `generate_data` in `model_select`, collects the returned p-values and displays a histogram of the p-values.

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -11,7 +11,26 @@ generate_data = function(n, p){
 model_select = function(covariances, responses, cutoff){
   m0 = lm(responses ~ covariances)
   lessCutoff = which(summary(m0)$p.value < cutoff)
+  if(length(lessCutoff) == 0) return(c())
   
   m1 = lm(responses ~ covariances[,lessCutoff])
   return(summary(m1)$p.value)
 }
+
+run_simulation = function(n_trials, n, p, cutoff){
+  for(i in 1:n_trials){
+    x = generate_data(n, p)
+    result = model_select(x$covariates, x$responses, cutoff)
+    hist(result)
+  }
+}
+
+run_simulation(1, 100, 10, 0.05)
+run_simulation(1, 100, 20, 0.05)
+run_simulation(1, 100, 50, 0.05)
+run_simulation(1, 1000, 10, 0.05)
+run_simulation(1, 1000, 20, 0.05)
+run_simulation(1, 1000, 50, 0.05)
+run_simulation(1, 10000, 10, 0.05)
+run_simulation(1, 10000, 20, 0.05)
+run_simulation(1, 10000, 50, 0.05)
